18b599a87a0e3ccfb63314404beb19a7da2a22e1
Lennart Breede <lennart.breede@gmail.com>
2026-02-02 22:09:16 -0800

    a virtual machine

--- src/main.c
+++ src/main.c
@@ -1,13 +1,27 @@
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, char *argv[]) {
+  initVM();
+
   Chunk chunk;
   initChunk(&chunk);
-  writeConstant(&chunk, 1.2, 123);
+  writeConstant(&chunk, 1, 123);
+  writeConstant(&chunk, 2, 123);
+  writeConstant(&chunk, 3, 123);
+  writeChunk(&chunk, OP_MULTIPLY, 123);
+  writeChunk(&chunk, OP_ADD, 123);
+  writeConstant(&chunk, 4, 123);
+  writeConstant(&chunk, 5, 123);
+  writeChunk(&chunk, OP_NEGATE, 123);
+  writeChunk(&chunk, OP_DIVIDE, 123);
+  writeChunk(&chunk, OP_SUBTRACT, 123);
   writeChunk(&chunk, OP_RETURN, 123);
 
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
