4c7dd616af2e4c1a2db6e1f52348d32291c84b6b
Lennart Breede <lennart.breede@gmail.com>
2026-02-04 21:59:21 -0800

    Scanning on demand

--- src/vm.c
+++ src/vm.c
@@ -1,5 +1,6 @@
 #include "vm.h"
 #include "chunk.h"
+#include "compiler.h"
 #include "debug.h"
 #include "value.h"
 #include <stdint.h>
@@ -79,8 +80,10 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
+  // vm.chunk = chunk;
+  // vm.ip = vm.chunk->code;
+  // return run();
 }
